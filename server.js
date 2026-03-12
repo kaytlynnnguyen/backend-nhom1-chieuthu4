3f9115e97e2c79a743de31f2206da27458a40121
Nguyen Gia Han <nguyenhan20048@gmail.com>
2026-03-12 13:09:46 +0700

    Tao Users

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
-const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
 
-const server = http.createServer((req, res) => {
-    res.write("Hello Backend");
-    res.end();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "pmweb_data"
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+db.connect((err)=>{
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Database connected");
+  }
+});
+
+app.get("/users", (req,res)=>{
+    db.query("SELECT * FROM users",(err,result)=>{
+        if(err){
+            res.send(err);
+        }else{
+            res.json(result);
+        }
+    });
+});
+
+app.get("/users/:id",(req,res)=>{
+    const id = req.params.id;
+
+    db.query("SELECT * FROM users WHERE id=?",[id],(err,result)=>{
+        if(err){
+            res.send(err);
+        }else{
+            res.json(result);
+        }
+    });
+});
+
+app.listen(5000, ()=>{
+  console.log("Server running on port 5000");
 });
